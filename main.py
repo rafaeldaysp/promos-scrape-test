1ac1419413f2dce98389ec4396cc818dba321cfa
Rafael <bboyrafinhazika@gmail.com>
2023-01-15 21:08:21 -0300

    update

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def criador_de_post(dados, op=0):
 
 def scraping(values, notebooks, url, i):
     headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
                 'Accept-Language': 'en-US, en;q=0.5'
     }
     try:
