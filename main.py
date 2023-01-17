27e9ac8f314f4655bbe400214d6a8c918568a958
Rafael <bboyrafinhazika@gmail.com>
2023-01-16 21:42:52 -0300

    update

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ def criador_de_post(dados, op=0):
 
 def scraping(values, notebooks, url, i):
     ua = UserAgent()
-    headers = {'upgrade-insecure-requests': '1', 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36', 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"', 'sec-ch-ua-mobile': '?0', 'sec-ch-ua-platform': '"Windows"', 'sec-fetch-site': 'none', 'sec-fetch-mod': '', 'sec-fetch-user': '?1', 'accept-encoding': 'gzip, deflate, br', 'accept-language': 'bg-BG,bg;q=0.9,en-US;q=0.8,en;q=0.7'}
+    headers = {'User-Agent':str(ua.chrome),
+           'Accept-Language': 'en-US, en;q=0.5'}
     try:
         response = requests.get(url[notebooks[i]], headers=headers)
         site = BeautifulSoup(response.text, 'html.parser')
